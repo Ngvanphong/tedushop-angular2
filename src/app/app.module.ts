73817f3547afeda8e79dcc64ec670aba31e00931
DESKTOP-2KTJBE5\NGUYEN VAN PHONG <ngvanphong2012@gmail.com>
2017-12-09 11:27:48 +0700

    12h09/12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {RouterModule} from '@angular/router';
+import {appRoute} from './app.routes';
 import { AppComponent } from './app.component';
-
+import {LoginModule} from './login/login.module';
+import {MainModule} from './main/main.module'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoute),
+    LoginModule,
+    MainModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main/user/user.module.ts
+++ src/app/main/user/user.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
+import {Routes,RouterModule} from '@angular/router';
 
+const userRoutes:Routes=[
+  {path:'', redirectTo:'index', pathMatch:'full'},
+  {path:'index', component:UserComponent, pathMatch:'full'}
+
+]
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(userRoutes)
+ 
   ],
   declarations: [UserComponent]
 })
