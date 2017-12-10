f4d4db9634fe94b68b0b15b318ab4af3cd6b4852
DESKTOP-2KTJBE5\NGUYEN VAN PHONG <ngvanphong2012@gmail.com>
2017-12-10 17:48:11 +0700

    123

--- src/app/main/main.routes.ts
+++ src/app/main/main.routes.ts
@@ -1,9 +1,8 @@
-import {Routes} from '@angular/router';
-import { MainComponent } from './main.component'
-
+import{Routes} from '@angular/router';
+import{MainComponent} from './main.component'
 export const mainRoutes:Routes=[
-    {path:'', component:MainComponent, children:[
-      {path:'', redirectTo:'home', pathMatch:'full'},
-      {path:'user', loadChildren:'./user/user.module#UserModule'}
-    ]}
+  {path:'', component:MainComponent, children:[
+    {path:'', redirectTo:'home', pathMatch:'full'},
+    {path:'user', loadChildren:'./user/user.module#UserModule'}
+  ]}
 ]
