c4f711753084a1c35ce00027c6c3018435bfabda
DESKTOP-2KTJBE5\NGUYEN VAN PHONG <ngvanphong2012@gmail.com>
2017-12-14 22:22:00 +0700

    11h14/12

---
+++ src/app/core/common/message.constant.ts
@@ -0,0 +1,9 @@
+export class MessageConstant{
+    public static SYSTEM_ERROR_MEG="Có lỗi kết nối đến máy chủ";
+    public static CONFIRM_DELETE_MEG="Bạn có muốn xóa bản ghi này?";
+    public static LOGIN_AGAIN_MEG="Bạn hết phiên đăng nhập, mời đăng nhập lại";
+    public static CREATE_OK_MEG="Thêm mới thành công";
+    public static UPDATE_OK_MEG="Cập nhật thành công";
+    public static DELETE_OK_MEG="Xóa thành công";
+
+}
---
+++ src/app/core/common/url.constant.ts
@@ -0,0 +1,4 @@
+export class UrlConstant{
+    public static lOGIN="/login";
+    public static HOME="/main/home/index"
+}
