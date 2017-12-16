80602ab80f243334d2f79112012a8cb0eadcd651
DESKTOP-2KTJBE5\NGUYEN VAN PHONG <ngvanphong2012@gmail.com>
2017-12-16 11:14:42 +0700

    11h16/12

--- src/app/core/service/data.service.ts
+++ src/app/core/service/data.service.ts
@@ -1,24 +1,67 @@
 import { Injectable } from '@angular/core';
-
+import {Http,Headers} from '@angular/http';
+import {AuthenService} from './authen.service'
+import {SystemConstant} from '../common/system.constant'
+import { Response } from '@angular/http/src/static_response';
+import {NotificationService} from './notification.service';
+import{MessageConstant } from '../common/message.constant'
+import{UtilityService} from './utility.service'
+import 'rxjs/add/operator/map';
+import {Router} from '@angular/router'
+import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class DataService {
 
-  constructor() { }
+  constructor(private _http:Http,private authentication:AuthenService, private notification:NotificationService, private utility:UtilityService) {
+    
+   }
+   private headers:Headers;
+
   get(uri:string){
-    return null;
+    this.headers.delete("Authenrization");
+    this.headers.append("Authenrization","Bearer"+this.authentication.getUserLogin().access_token)
+    return this._http.get(SystemConstant.BASE_API+uri,{headers:this.headers}).map(this.extractData)
   };
   post(uri:string, data?: any){
-    return null;
+    this.headers.delete("Authenrization");
+    this.headers.append("Authenrization","Bearer"+this.authentication.getUserLogin().access_token)
+    return this._http.post(SystemConstant.BASE_API+uri,data,{headers:this.headers}).map(this.extractData)
   };
   put(uri:string,data?:any){
-    return null;
+    this.headers.delete("Authenrization");
+    this.headers.append("Authenrization","Bearer"+this.authentication.getUserLogin().access_token)
+    return this._http.put(SystemConstant.BASE_API+uri,{headers:this.headers}).map(this.extractData)
   };
   delete(uri:string,key:string,id:string){
-    return null;
+    this.headers.delete("Authenrization");
+    this.headers.append("Authenrization","Bearer"+this.authentication.getUserLogin().access_token)
+    return this._http.get(SystemConstant.BASE_API+uri+"/?"+key+"="+id,{headers:this.headers}).map(this.extractData)
   };
   postFile(uri:string,data?:any){
-    
+    let newHeaders= new Headers();
+    newHeaders.append("Authenrization","Bearer"+this.authentication.getUserLogin().access_token)
+    return this._http.get(SystemConstant.BASE_API+uri,{headers:this.headers}).map(this.extractData)
   }
+  private extractData(res:Response){
+   let body = res.json();
+    return body||{};
+  }
+  public handleError(error: any) {
+    if (error.status == 401) {
+        localStorage.removeItem(SystemConstant.CURRENT_USER);
+        this.notification.printErrorMessage(MessageConstant.LOGIN_AGAIN_MEG);
+        this.utility.navigateToLogin();
+    }
+    else {
+        let errMsg = (error.message) ? error.message :
+            error.status ? `${error.status} - ${error.statusText}` : 'Lỗi hệ thống';
+        this.notification.printErrorMessage(errMsg);
+
+        return Observable.throw(errMsg);
+    }
+
+}
+}
 
 
 }
