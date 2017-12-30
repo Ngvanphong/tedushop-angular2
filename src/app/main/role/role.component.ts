051706726f65f3bee227602526ba1756127764a2
DESKTOP-2KTJBE5\NGUYEN VAN PHONG <ngvanphong2012@gmail.com>
2017-12-30 09:42:29 +0700

    30/12

--- src/app/main/role/role.component.ts
+++ src/app/main/role/role.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit,ViewChild} from '@angular/core';
 import {DataService} from '../../core/service/data.service'
 import { ModalDirective } from 'ngx-bootstrap/modal';
 import { SystemConstant } from '../../core/common/system.constant';
+import {NotificationService} from '../../core/service/notification.service'
+import {MessageConstant} from '../../core/common/message.constant'
 @Component({
   selector: 'app-role',
   templateUrl: './role.component.html',
@@ -17,7 +19,7 @@ export class RoleComponent implements OnInit {
   public entity:any;
   roles:any;
 
-  constructor(private dataservice: DataService) { }
+  constructor(private dataservice: DataService, private _notification:NotificationService) { }
 
   ngOnInit() {
     this.load();
@@ -43,11 +45,12 @@ export class RoleComponent implements OnInit {
   saveChanged(valid:boolean){
     if(valid){
       if (this.entity.Id==undefined){
-        this.dataservice.post("api/appRole/add",JSON.stringify(this.entity)).subscribe((res:any)=>{
+        this.dataservice.post("/api/appRole/add",JSON.stringify(this.entity)).subscribe((res:any)=>{
           this.load();
           this.addEditModal.hide();
-        }
-        )
+          this._notification.printSuccesMessage(MessageConstant.CREATE_OK_MEG);
+
+        },error=>this.dataservice.handleError(error))
       }
       else{
 
