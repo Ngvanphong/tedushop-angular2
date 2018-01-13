acc4e7d06311009fbfcd263c6f251c59a6e05f30
DESKTOP-2KTJBE5\NGUYEN VAN PHONG <ngvanphong2012@gmail.com>
2018-01-13 08:40:18 +0700

    13/01

--- src/app/main/function/function.component.ts
+++ src/app/main/function/function.component.ts
@@ -1,15 +1,80 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TreeComponent } from 'angular-tree-component/dist/components/tree.component';
+import { DataService } from '../../core/service/data.service';
+import { UtilityService } from '../../core/service/utility.service';
+import { NotificationService } from '../../core/service/notification.service'
+import { ModalDirective } from 'ngx-bootstrap/modal';
+import{MessageConstant} from '../../core/common/message.constant'
 @Component({
   selector: 'app-function',
   templateUrl: './function.component.html',
   styleUrls: ['./function.component.css']
 })
 export class FunctionComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild(TreeComponent) private treeFunction: TreeComponent
+  @ViewChild('addEditModal') private addEditModal:ModalDirective;
+  public _functions: any[];
+  public _functionHierachy: any[];
+  public filter: string = "";
+  public entity:any;
+  public editFlag:boolean;
+  constructor(private _dataService: DataService, private utilityService: UtilityService, private _notificationService:NotificationService) { }
 
   ngOnInit() {
+    this.search(event);
   }
+ public addEdit(){
+   this.entity={};
+   this.addEditModal.show();
+  this.editFlag=false;
+ }
+
+  public search(event) {
+    this._dataService.get('/api/function/getall?filter=' + this.filter)
+      .subscribe((response: any[]) => {
+        console.log(response)
+        this._functions = response.filter(x => x.ParentId == null);
+        this._functionHierachy = this.utilityService.Unflatten(response);
+      }, error => this._dataService.handleError(error));
+  };
+  public showEdit(id: string) {
+    this._dataService.get("/api/function/detail/"+id).subscribe((res:any)=>{
+      this.entity=res;
+      this.editFlag=true;
+      this.addEditModal.show();
+    },error=>this._dataService.handleError(error))
+  };
+
+  private deleteConfirm(id:any){
+    this._dataService.delete("/api/function/delete",'id',id).subscribe((res:any)=>{
+      this._notificationService.printSuccesMessage(MessageConstant.DELETE_OK_MEG);
+      this.search(event);
+    },error=>this._dataService.handleError(error))
+  }
+  public delete(id:any){
+    this._notificationService.printConfirmationDialog(MessageConstant.CONFIRM_DELETE_MEG,()=>this.deleteConfirm(id))
+  }
+
+ public saveChanges(valid:boolean){
+    if(valid){
+      if(this.editFlag){
+        this._dataService.put("/api/function/update",JSON.stringify(this.entity)).subscribe(res=>{
+          this.search(event);
+          this.addEditModal.hide();
+          this._notificationService.printSuccesMessage(MessageConstant.UPDATE_OK_MEG)
+        },error=>this._dataService.handleError(error))
+      }
+      else{
+        this._dataService.post("/api/function/add",JSON.stringify(this.entity)).subscribe(res=>{
+          this.addEditModal.hide();
+          this._notificationService.printSuccesMessage(MessageConstant.CREATE_OK_MEG);
+          this.search(event);
+        },error=>this._dataService.handleError(error))
+
+      }
+    }
+
+ }
+
 
 }
--- src/app/main/function/function.module.ts
+++ src/app/main/function/function.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FunctionComponent } from './function.component';
+import { TreeModule } from 'angular-tree-component';
+import {Routes,RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {DataService} from '../../core/service/data.service';
+import {UtilityService} from '../../core/service/utility.service';
+import {NotificationService} from '../../core/service/notification.service';
+import { ModalModule } from 'ngx-bootstrap/modal';
+const routerFunction:Routes=[
+  {path:'',redirectTo:'index', pathMatch:'full'},
+  {path:'index',component:FunctionComponent}
+]
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    TreeModule ,
+    FormsModule,
+    ModalModule,
+    RouterModule.forChild(routerFunction),
+    ModalModule.forRoot(),
+  ],
+  providers: [
+    DataService,
+    UtilityService,
+    NotificationService
   ],
   declarations: [FunctionComponent]
 })
