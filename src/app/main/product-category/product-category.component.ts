d0d7d6345c8ebe10d5fb22500d7e894d986b729c
PHONGNGUYEN\Nguyenvan <ngvanphong2012@gamil.com>
2018-03-12 21:03:35 +0700

    180312_productCategory

--- src/app/main/product-category/product-category.component.ts
+++ src/app/main/product-category/product-category.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MessageConstant} from '../../core/common/message.constant';
+import {DataService} from '../../core/service/data.service';
+import {UtilityService} from '../../core/service/utility.service';
+import {NotificationService} from '../../core/service/notification.service';
+import {ModalDirective} from 'ngx-bootstrap/modal';
+import {TreeComponent} from 'angular-tree-component';
+
 
 @Component({
   selector: 'app-product-category',
@@ -7,9 +14,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCategoryComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(TreeComponent) private treeProductCategory:TreeComponent;
+  @ViewChild('addEditModal') private addEditModal:ModalDirective;
+  public filter:string ='';
+  public functionId:string;
+  public entity:any;
+  public _productCategoryHierachy:any[];
+  public _productCategories:any[];
+  constructor(private _dataService:DataService, private notificationService:NotificationService, private utilitiService:UtilityService ) {
+
+    }
 
   ngOnInit() {
+    this.search();
+    this.getListForDropdown();
   }
+
+  public createAlias(){
+    this.entity.Alias=this.utilitiService.MakeSeoTitle(this.entity.Name);
+  }
+  // loadData
+  public search(){
+    this._dataService.get('/api/productCategory/getall?filter='+this.filter).subscribe((res:any[])=>{
+      this._productCategoryHierachy=this.utilitiService.Unflatten2(res);
+      this._productCategories=res;
+    },error=>this._dataService.handleError(error));
+  }
+
+  public getListForDropdown(){
+    this._dataService.get('/api/productCategory/getallhierachy').subscribe((res:any[])=>{
+      this._productCategories=res;
+    },error=>this._dataService.handleError(error));
+  }
+// show add
+  public showAdd(){
+    this.entity={};
+    this.addEditModal.show();
+
+  }
+// show Edit Form
+  public showEdit(id:string){
+    this._dataService.get('/api/productCategory/detail/'+id).subscribe((res:any)=>{
+      this.entity=res;
+      this.addEditModal.show();
+    },error=>this._dataService.handleError(error));
+  }
+
+  //Delete Confirm
+
+  private deleteConfirm(id:string){
+    this._dataService.delete('/api/productCategory/delete','id',id).subscribe((res:any)=>{
+      this.notificationService.printSuccesMessage(MessageConstant.DELETE_OK_MEG);
+      this.search();
+    },error=>this._dataService.handleError(error));
+  }
+
+  //action delete
+
+  public delete(id:string){
+    this.notificationService.printConfirmationDialog(MessageConstant.CONFIRM_DELETE_MEG,()=>this.deleteConfirm(id))
+  }
+
+  //Save change for modal
+  public saveChanges(valid:boolean){
+      if(valid){
+        if(this.entity.ID===undefined){
+          this._dataService.post('/api/productCategory/add',this.entity).subscribe((res:any)=>{
+            this.notificationService.printSuccesMessage(MessageConstant.CREATE_OK_MEG);
+            this.search();
+            this.addEditModal.hide();
+          },error=>this._dataService.handleError(error));
+        }
+        else{
+          this._dataService.put('/api/productCategory/update',this.entity).subscribe((res:any)=>{
+            this.notificationService.printSuccesMessage(MessageConstant.UPDATE_OK_MEG);
+            this.search();
+            this.addEditModal.hide();        
+          },error=>this._dataService.handleError(error));
+        }
+
+      }
+  }
+
+  public onSelectedChange($event) {
+    console.log($event);
+  }
+  
+
+
 
 }
--- src/app/main/product-category/product-category.module.ts
+++ src/app/main/product-category/product-category.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCategoryComponent } from './product-category.component';
-
+import {Routes,RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {TreeModule} from 'angular-tree-component';
+import { ModalModule } from 'ngx-bootstrap';
+import {DataService} from '../../core/service/data.service';
+import {UtilityService} from '../../core/service/utility.service'
+import {NotificationService} from '../../core/service/notification.service'
+const productCategoryRoute:Routes=[
+  {path:'', redirectTo:'index', pathMatch:'full'},
+  {path:'index',component:ProductCategoryComponent}
+]
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(productCategoryRoute),
+    TreeModule,
+    ModalModule,
+    FormsModule,
+    ModalModule.forRoot(),
+
+  ],
+  providers:[
+    DataService,
+    UtilityService,
+    NotificationService
   ],
   declarations: [ProductCategoryComponent]
 })
