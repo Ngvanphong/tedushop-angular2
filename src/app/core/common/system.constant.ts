7e440542ac6732c00d1d910b31ae972a05064bff
PHONGNGUYEN\Nguyenvan <ngvanphong2012@gamil.com>
2018-03-12 19:04:48 +0700

    123

--- src/app/core/common/system.constant.ts
+++ src/app/core/common/system.constant.ts
@@ -1,5 +1,5 @@
 export class SystemConstant{
     public static CURRENT_USER="CURRENT_USER";
     //public static BASE_API = "http://tedushopapi.tedu.com.vn";
-    public static BASE_API="http://localhost:5000"
+    public static BASE_API="http://localhost:55429"
 }
