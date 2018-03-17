f7ced6536d7c7e8ee065a44425c7dfa014972f02
PHONGNGUYEN\Nguyenvan <ngvanphong2012@gamil.com>
2018-03-17 15:56:37 +0700

    16h17/3

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -123,9 +123,7 @@ $SIDEBAR_MENU.find('a[href="' + CURRENT_URL + '"]').parent('li').addClass('curre
 
 $SIDEBAR_MENU.find('a').filter(function () {
 	return this.href == CURRENT_URL;
-}).parent('li').addClass('current-page').parents('ul').slideDown(function() {
-	setContentHeight();
-}).parent().addClass('active');
+})
 
 // recompute content when resizing
 $(window).smartresize(function(){  
